d4e5ab09094ea23dad33878b9635905732abf364
serejqka <serejaseed@yandex.ru>
2023-05-20 18:48:54 +0700

    change: create Headet, crate MainPage, edit App

---
+++ src/Page/MainPage/MainPage.jsx
@@ -0,0 +1,13 @@
+import { memo } from 'react';
+
+import Header from '../../Components/Header/Header';
+
+const MainPage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default memo(MainPage);
